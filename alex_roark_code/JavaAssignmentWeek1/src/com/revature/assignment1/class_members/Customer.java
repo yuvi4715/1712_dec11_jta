46ceae864146d29d5199764cb522f7d8040a32ec
Alex Roark <thealexroark@gmail.com>
2017-12-12 10:07:26 -0500

    implemented Customer.java and CustomerCreator.java

--- alex_roark_code/JavaAssignmentWeek1/src/com/revature/assignment1/class_members/Customer.java
+++ alex_roark_code/JavaAssignmentWeek1/src/com/revature/assignment1/class_members/Customer.java
@@ -2,6 +2,51 @@ package com.revature.assignment1.class_members;
 
 public class Customer {
 	
+	private String name;
+	private int orders;
+	public static final int maxOrders = 100;
+	public static String storeName = "Nathan's Supermarket";
 	
+	public Customer() {
+		this.name = "Bob Martin";
+		this.orders = 3;
+	}
+	
+	public Customer(String name, int orders) {
+		this.name = name;
+		this.orders = orders;
+	}
+	
+	public void setName() {
+		this.name = "Richard Martin";
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setOrderNumber() {
+		this.orders = 1;
+	}
+	
+	public void setOrderNumber(int orders) {
+		this.orders = orders;
+	}
+	
+	public int getOrders() {
+		return this.orders;
+	}
+	
+	public static int getMaxOrders() {
+		return maxOrders;
+	}
+	
+	public static String getStoreName() {
+		return storeName;
+	}
 
 }
--- alex_roark_code/JavaAssignmentWeek1/src/com/revature/assignment1/class_members/CustomerCreator.java
+++ alex_roark_code/JavaAssignmentWeek1/src/com/revature/assignment1/class_members/CustomerCreator.java
@@ -4,7 +4,26 @@ public class CustomerCreator {
 
 	public static void main(String[] args) {
 
+		Customer defaultCustomer = new Customer();
+		Customer newCustomer = new Customer("Anthony Martin", 5);
+
+		defaultCustomer.setName();
+		newCustomer.setName("Michelle Martin");
+
+		System.out.println("Name for the defaultCustomer object is " + defaultCustomer.getName());
+		System.out.println("Name for the newCustomer object is " + newCustomer.getName());
+
+		defaultCustomer.setOrderNumber();
+		newCustomer.setOrderNumber(9);
+
+		System.out.println("Orders for the defaultCustomer object is " + defaultCustomer.getOrders());
+		System.out.println("Orders for the newCustomer object is " + newCustomer.getOrders());
+
+		int maxOrders = Customer.getMaxOrders();
+		String storeName = Customer.getStoreName();
 		
+		System.out.println("Max orders for a customer is " + maxOrders);
+		System.out.println("Store name for the customers is " + storeName);
 	}
 
 }
