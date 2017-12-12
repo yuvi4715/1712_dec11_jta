be4e4679100a77496bb4af4207f541adfa4c7d92
Alex Roark <thealexroark@gmail.com>
2017-12-11 19:16:28 -0500

    add Q1.java and Q2.java and Q3.java

---
+++ alex_roark_code/JavaAssignmentWeek1/src/com/revature/assignment1/Q2.java
@@ -0,0 +1,25 @@
+package com.revature.assignment1;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+
+		int number1 = 3;
+		int number2 = 6;
+		int number3 = 9;
+		
+		int greatestNumber = 0;
+		if (number1 > number2) {
+			if (number1 > number3) {
+				greatestNumber = number1;
+			}
+		} else if (number2 > number3) {
+			greatestNumber = number2;
+		} else {
+			greatestNumber = number3;
+		}
+		
+		System.out.println("The greatest number is " + greatestNumber);
+	}
+
+}
