ca0c5cfe7290ce4f4cfb5bc1fe4cd02fe9728e8b
Alex Roark <thealexroark@gmail.com>
2017-12-12 15:09:48 -0500

    modify Q12.java and Q13.java and add Q14.java

--- alex_roark_code/JavaAssignmentWeek1/src/com/revature/assignment1/exceptions/Q12.java
+++ alex_roark_code/JavaAssignmentWeek1/src/com/revature/assignment1/exceptions/Q12.java
@@ -4,19 +4,19 @@ public class Q12 {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 10; i++) {
-			try {
-				System.out.println("Starting try block");
+		try {
+			System.out.println("Starting try block");
+			for (int i = 0; i < 10; i++) {
 				if (i == 5) {
 					throw new Q10_CustomException("Loop counter equals 5!");
 				}
-				System.out.println("Ending try block");
 			}
-			catch (Q10_CustomException customException) {
-				System.out.println("Starting catch block");
-				System.out.println(customException.getMessage());
-				System.out.println("Ending catch block");
-			}
+			System.out.println("Ending try block");
+		}
+		catch (Q10_CustomException customException) {
+			System.out.println("Starting catch block");
+			System.out.println(customException.getMessage());
+			System.out.println("Ending catch block");
 		}
 	}
 }
--- alex_roark_code/JavaAssignmentWeek1/src/com/revature/assignment1/exceptions/Q14.java
+++ alex_roark_code/JavaAssignmentWeek1/src/com/revature/assignment1/exceptions/Q14.java
@@ -1,14 +1,14 @@
 package com.revature.assignment1.exceptions;
 
-public class Q13 {
+public class Q14 {
 
 	public static void main(String[] args) {
 
 		try {
 			System.out.println("Starting try block");
 			for (int i = 0; i < 10; i++) {
-				if (i == 20) {
-					throw new Q10_CustomException("Loop counter equals 20!");
+				if (i == 5) {
+					throw new Q10_CustomException("Loop counter equals 5!");
 				}
 			}
 			System.out.println("Ending try block");
@@ -17,6 +17,9 @@ public class Q13 {
 			System.out.println("Starting catch block");
 			System.out.println(customException.getMessage());
 			System.out.println("Ending catch block");
+		}
+		finally {
+			System.out.println("Exception has been caught. Goodbye!");
 		}
 	}
 }
