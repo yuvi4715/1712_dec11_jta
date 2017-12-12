95126a3f72638d6e464db0eb560415a6f86f9033
Alex Roark <thealexroark@gmail.com>
2017-12-12 14:03:01 -0500

    rename Q10.java to Q10_CustomException.java and add Q11_ExceptionThrower.java

--- alex_roark_code/JavaAssignmentWeek1/src/com/revature/assignment1/exceptions/Q10_CustomException.java
+++ alex_roark_code/JavaAssignmentWeek1/src/com/revature/assignment1/exceptions/Q10_CustomException.java
@@ -1,7 +1,12 @@
 package com.revature.assignment1.exceptions;
 
-public class Q10 extends Exception {
-	
+public class Q10_CustomException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public String getMessage() {
 		return "An Exception has occurred!";
---
+++ alex_roark_code/JavaAssignmentWeek1/src/com/revature/assignment1/exceptions/Q11_ExceptionThrower.java
@@ -0,0 +1,9 @@
+package com.revature.assignment1.exceptions;
+
+public class Q11_ExceptionThrower {
+	
+	public Q11_ExceptionThrower() throws Q10_CustomException {
+		throw new Q10_CustomException();
+	}
+
+}
