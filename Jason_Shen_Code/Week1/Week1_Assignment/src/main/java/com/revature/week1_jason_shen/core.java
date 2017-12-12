7d88a6901cd42e922282b0e9f32eced8fa5c2b05
cwjshen <shenj529@gmail.com>
2017-12-11 19:22:48 -0500

    questions 1-3

--- Jason_Shen_Code/Week1/Week1_Assignment/src/main/java/com/revature/week1_jason_shen/core.java
+++ Jason_Shen_Code/Week1/Week1_Assignment/src/main/java/com/revature/week1_jason_shen/core.java
@@ -7,7 +7,7 @@ package com.revature.week1_jason_shen;
 
 public class core {
 	
-	public static void question1() {
+	static void basic_ops() {
 	// 1. Write a program to add, subtract, multiply and divide two non-zero hard-coded numbers.
 		
 		// Hard coded numbers
@@ -20,17 +20,56 @@ public class core {
 		int prod = x * y;
 		int quot = x / y;
 		
-		System.out.println("1. Hard Coded: " + x + " " + y);
+		System.out.println("Question 1: ");
+		System.out.println("-----");
+		System.out.println("Hard Coded: " + x + " " + y);
 		System.out.println("Sum: " + sum);
 		System.out.println("Difference: " + diff);
 		System.out.println("Product: " + prod);
 		System.out.println("Quotient: " + quot);
+		System.out.println();
+		
+		return;
 	}
 	
-	public static void main(String args[]) {
+	static int find_greatest(int x, int y, int z) {
+	// 2. Write a program to search for the greatest of three numbers using Short-circuit Operators and print the result. 
+		
+		// Short circuit operators == ternary operators?
+		
+		// A number is the greatest iff greater than all other numbers
+		// Both comparison statements within a clause should be true to find the greatest number
+		int greatest = (x > y) && (x > z) ? x : (y > x) && (y > z) ? y : z;
+		
+		System.out.println("Question 2: ");
+		System.out.println("-----");
+		System.out.println("Greatest of " + x + " " + y + " " + z + ": " + greatest);
+		System.out.println();
+		
+		return greatest;
+			
+	}
 	
-		question1();
-	
+	static void check_bools() {
+	// 3. Write a program declare two variables a & b and initialize them to true and false respectively. 
+	//    Get the output of the following computations:
+	//	  !a, a | b, (!a & b) | (a & !b)		
+		
+		// Declare bools
+		boolean a = true;
+		boolean b = false;
+		
+		System.out.println("Question 3: ");
+		System.out.println("-----");
+		
+		// Compute and print
+		System.out.println("!a: " + !a);
+		System.out.println("(a | b): " + (a | b));
+		System.out.println("(!a & b) | (a & !b): " + ((!a & b) | (a & !b)));
+		System.out.println();
+		
+		return;		
+			
 	}
 	
 }
---
+++ Jason_Shen_Code/Week1/Week1_Assignment/src/main/java/com/revature/week1_jason_shen/solution.java
@@ -0,0 +1,15 @@
+package com.revature.week1_jason_shen;
+
+public class solution {
+	public static void main(String args[]) {
+		
+		// Core Java: Questions 1-3
+		core.basic_ops();
+		core.find_greatest(4,5,6);
+		core.check_bools();
+		
+		// Syntax: Questions 4-6 
+	
+	}
+	
+}
