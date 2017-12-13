0a2dc1f6f6043ad789c04a98b2215710201480a8
Alex Roark <thealexroark@gmail.com>
2017-12-13 12:18:35 -0500

    modify Q2.java

--- alex_roark_code/JavaAssignmentWeek1/src/com/revature/assignment1/core_java/Q2.java
+++ alex_roark_code/JavaAssignmentWeek1/src/com/revature/assignment1/core_java/Q2.java
@@ -7,18 +7,16 @@ public class Q2 {
 		int number1 = 3;
 		int number2 = 6;
 		int number3 = 9;
-		
+
 		int greatestNumber = 0;
-		if (number1 > number2) {
-			if (number1 > number3) {
-				greatestNumber = number1;
-			}
+		if (number1 > number2 && number1 > number3) {
+			greatestNumber = number1;
 		} else if (number2 > number3) {
 			greatestNumber = number2;
 		} else {
 			greatestNumber = number3;
 		}
-		
+
 		System.out.println("The greatest number is " + greatestNumber);
 	}
 
