21e1042fc0ffe2ec4037e812d4d9db8c798cbbad
Tuan Pham <tuandpham75@gmail.com>
2017-12-15 00:32:52 -0500

    Modified code for emphasis and readability

--- Tuan_Pham_Code/TuanPhamJavaAssignment/src/main/java/com/revature/question78/Customer.java
+++ Tuan_Pham_Code/TuanPhamJavaAssignment/src/main/java/com/revature/question78/Customer.java
@@ -14,42 +14,79 @@ public class Customer {
 	final String company = "Revature";
 	public String name;
 	public int age;
-	static int balance = 100;
+	static int balance;
+	static int customerCount= 0;
+	static int hours = 0;
 	
 
-	public Customer() {
+	public Customer(String name) {
+		this.name = name;
+		customerCount++;
 	}
 	public Customer(String name, int age, int balance) {
+		this.name = name;
+		this.age = age;
 		Customer.balance = balance;
+		customerCount++;
+		
 	}
 	
+	// Print how many customers there are
+	public static void printCustomerCount() {
+		System.out.print(customerCount);
+	}
+	
+	// Print a customer's balance
 	public static void printBalance() {
-		System.out.println("balance is " + balance);
+		System.out.println("Cash card has " + balance);
 	}
 	
-	public static void addBalance() {
+	// Default add 100 for to cash card during pay
+	public void addBalance() {
 		System.out.println("Adding 100 to balance");
 		balance += 100;
-		System.out.println("balance is now " + balance);
-	}
-	public void printBalance(int newBalance) {
-		System.out.println("Custom balance is " + newBalance);
+		System.out.println("Cash card now has " + balance);
 	}
 	
+	// Overload addBalance to add extra money from bonus
 	public void addbalance(int additionalBalance) {
 		System.out.println("Adding " + additionalBalance + " to balance");
 		
 		balance += additionalBalance;
-		System.out.println("balance is now " + balance);
+		System.out.println("Cash card now has " + balance);
+	}
+	
+	// Keep track of hours. Default is 8 hours a day
+	public void logHours() {
+		hours = 8;
+		System.out.println("Worked 8 hours");
+	}
+	
+	// Keep track of overtime hours by overloading logHours() method
+	public void logHours(int overtime) {
+		hours = 8 + overtime;
+		System.out.println("Worked " + hours + " hours including " + overtime+ " hours overtime");
 	}
 	
 	public static void main(String[] args) {
-		printBalance();
-		addBalance();
+		//printBalance();
+		//addBalance();
 		
-		Customer c1 = new Customer();
-		c1.printBalance(500);
-		c1.addbalance(50);
+		Customer c1 = new Customer("Jake Perez");
+		System.out.println(c1.name);
+		printBalance();
+		c1.logHours(2);
+		c1.addbalance(140);
+		System.out.println("");
+		
+		Customer c2 = new Customer("Jose Rodiguez", 20, 0);
+		System.out.println(c2.name);
+		printBalance();
+		c2.logHours();
+		c2.addBalance();
+		
+		System.out.println("\nTotal number of customers: ");
+		printCustomerCount();
 	}
 	
 }
--- Tuan_Pham_Code/TuanPhamJavaAssignment/src/main/java/com/revature/question78/Question8.java
+++ Tuan_Pham_Code/TuanPhamJavaAssignment/src/main/java/com/revature/question78/Question8.java
@@ -9,14 +9,14 @@ b.	Uses each of the Customer class members
 public class Question8 {
 
 	public static void main(String[] args) {
-		Customer c1 = new Customer();
-		
-		c1.printBalance(10);
-		System.out.println("Customer 1's original balance was " + c1.balance);
-		
-		Customer c2 = new Customer();
-		System.out.println("Customer 2's balance is " + c2.balance);
-		c2.addbalance(50);
+//		Customer c1 = new Customer();
+//		
+//		c1.printBalance(10);
+//		System.out.println("Customer 1's original balance was " + c1.balance);
+//		
+//		Customer c2 = new Customer();
+//		System.out.println("Customer 2's balance is " + c2.balance);
+//		c2.addbalance(50);
 
 		
 		
