7c0f2dc41f4c75a86bde3248f8d2d80ef7ce1e74
DFM <dfmai@ucdavis.edu>
2017-12-17 21:53:56 -0500

    finished bonus questions

--- code/hw1_java/src/bonus/Livelock.java
+++
@@ -1,5 +0,0 @@
-package bonus;
-
-public class Livelock {
-
-}
---
+++ code/hw1_java/src/bonus/Resource.java
@@ -0,0 +1,11 @@
+package bonus;
+
+import bonus.Worker;
+
+public class Resource {
+    Worker owner;
+
+    public Resource (Worker d) {
+        owner = d;
+    }
+}
