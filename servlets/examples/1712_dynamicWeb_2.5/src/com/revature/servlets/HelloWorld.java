eb4ca496e73c0d5ab1d115a08c68a7f16a27f34d
Yuvi <yuvi@revature.com>
2018-01-04 12:10:06 -0500

    servlet examples

---
+++ servlets/examples/1712_dynamicWeb_2.5/src/com/revature/servlets/HelloWorld.java
@@ -0,0 +1,31 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public HelloWorld() {
+        super();
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init");
+	}
+
+	public void destroy() {
+		System.out.println("destroy");
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("service");
+	}
+	
+}
