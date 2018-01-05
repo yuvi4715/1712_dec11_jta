65086af8e85ee4db23f2e5a499092e988fba6e5a
Clayton Su <csu007@ucr.edu>
2018-01-04 21:35:59 -0500

    login connects to servlet

---
+++ Clayton_Su_Code/ers/src/main/java/ers/Reimburse_Closed.java
@@ -0,0 +1,53 @@
+package ers;
+
+public class Reimburse_Closed
+{
+	private int rid_closed;
+	private int amount;
+	private String status;
+	private int emp_id;
+	private int mana_id;
+	
+	public int getRid_closed()
+	{
+		return rid_closed;
+	}
+	public void setRid_closed(int rid_closed)
+	{
+		this.rid_closed = rid_closed;
+	}
+	public int getAmount()
+	{
+		return amount;
+	}
+	public void setAmount(int amount)
+	{
+		this.amount = amount;
+	}
+	public String getStatus()
+	{
+		return status;
+	}
+	public void setStatus(String status)
+	{
+		this.status = status;
+	}
+	public int getEmp_id()
+	{
+		return emp_id;
+	}
+	public void setEmp_id(int emp_id)
+	{
+		this.emp_id = emp_id;
+	}
+	public int getMana_id()
+	{
+		return mana_id;
+	}
+	public void setMana_id(int mana_id)
+	{
+		this.mana_id = mana_id;
+	}
+	
+	
+}
---
+++ Clayton_Su_Code/ers/src/main/java/ers/User.java
@@ -0,0 +1,44 @@
+package ers;
+
+public class User
+{
+	private int eid;
+	private String firstname;
+	private String lastname;
+	private String isManager;
+	
+	public int getEid()
+	{
+		return eid;
+	}
+	public void setEid(int eid)
+	{
+		this.eid = eid;
+	}
+	public String getFirstname()
+	{
+		return firstname;
+	}
+	public void setFirstname(String firstname)
+	{
+		this.firstname = firstname;
+	}
+	public String getLastname()
+	{
+		return lastname;
+	}
+	public void setLastname(String lastname)
+	{
+		this.lastname = lastname;
+	}
+	public String getIsManager()
+	{
+		return isManager;
+	}
+	public void setIsManager(String isManager)
+	{
+		this.isManager = isManager;
+	}
+	
+	
+}
