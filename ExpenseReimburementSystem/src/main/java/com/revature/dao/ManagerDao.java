4dd04a703ce8ea7077be0d5342538b13f8102f7d
Tuan Pham <tuandpham75@gmail.com>
2018-01-15 19:27:51 -0500

    cleaning repo

--- ExpenseReimburementSystem/src/main/java/com/revature/dao/ManagerDao.java
+++
@@ -1,8 +0,0 @@
-package com.revature.dao;
-
-import com.revature.model.Employee;
-import com.revature.model.Reimbursement;
-
-public interface ManagerDao {
-	public void approve(int id, int managerID);
-}
